07d0395b7be04af0211e04e0e9f8005ebba03806
Yiwen Zhou <even.zhou@mail.utoronto.ca>
2018-11-06 05:28:33 -0500

    Add ScoreBoard functionality.

--- Phase1/GameCentre/app/src/main/java/group_0550/gamecentre/ScoreBoard.java
+++ Phase1/GameCentre/app/src/main/java/group_0550/gamecentre/ScoreBoard.java
@@ -1,45 +1,47 @@
 package group_0550.gamecentre;
 
-import java.util.Observable;
-import java.util.Observer;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
 
-public class ScoreBoard implements Observer {
+public class ScoreBoard implements Serializable {
+    private HashMap<String, Integer> userScoreMap;
 
-    /**
-     * the board to play.
-     */
-    private Board b;
-
-    /**
-     * the total steps that the player played.
-     */
-    private int TotalSteps = 1;
-
-    /**
-     * the Total Score that will be divided by total steps.
-     */
-    private int TotalScore = 10000;
-
-
-    /***
-     * initialise the score board.
-     * @param b the board to play with.
-     */
-    public ScoreBoard(Board b){
-        this.b = b;
-        this.b.addObserver(this);
+    public ScoreBoard() {
+        this.userScoreMap = new HashMap<>();
     }
 
-    @Override
-    public void update(Observable o, Object arg) {
-        TotalSteps++;
+    public void addScore(String username, Integer newScore) {
+        this.userScoreMap.putIfAbsent(username, 0);
+        Integer currentScore = this.userScoreMap.get(username);
+        if (currentScore < newScore) {
+            this.userScoreMap.replace(username, newScore);
+        }
     }
 
-    /**
-     * get the final score.
-     * @return total score divide by total steps.
-     */
-    public int getTotalScore() {
-        return TotalScore / TotalSteps;
+    public String getHighestScore(String username) {
+        return this.userScoreMap.get(username).toString();
     }
+
+    public ArrayList<String> getHighest5Scores() {
+        Set keys = this.userScoreMap.keySet();
+        Iterator iterkeys = keys.iterator();
+        ArrayList<String> scores = new ArrayList<>();
+        while (iterkeys.hasNext()) {
+            String key = (String) iterkeys.next();
+            String score = this.userScoreMap.get(key).toString() +
+                    ',' + key;
+            scores.add(score);
+            System.out.println(scores);
+        }
+        Collections.sort(scores, Collections.<String>reverseOrder());
+        for (int i = 0; i <= 5; i++) {
+            scores.add(new Integer(i).toString(i) + ",Nobody" + new Integer(i).toString(i));
+        }
+        return new ArrayList<>(scores.subList(0, 5));
+    }
+
 }
