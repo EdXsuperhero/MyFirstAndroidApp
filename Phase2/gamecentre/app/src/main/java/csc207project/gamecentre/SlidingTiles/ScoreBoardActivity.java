7fe4f996b56ba4e223e1baa016ff81f36838f958
Yiwen Zhou <even.zhou@mail.utoronto.ca>
2018-11-27 21:08:44 -0500

    Add sign in fragment layout

--- Phase2/gamecentre/app/src/main/java/csc207project/gamecentre/SlidingTiles/ScoreBoardActivity.java
+++ Phase2/gamecentre/app/src/main/java/csc207project/gamecentre/SlidingTiles/ScoreBoardActivity.java
@@ -20,7 +20,7 @@ import java.util.List;
 import java.util.Map;
 
 import csc207project.gamecentre.R;
-import csc207project.gamecentre.MainMenu.LoginActivity;
+import csc207project.gamecentre.MainMenu.LoginFragment.LoginActivity;
 
 /**
  * The scoreboard activity for sliding tiles.
