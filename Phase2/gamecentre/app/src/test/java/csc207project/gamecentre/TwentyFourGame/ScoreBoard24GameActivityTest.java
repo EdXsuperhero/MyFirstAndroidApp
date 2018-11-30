c5695948d4a67d20a7f9c6192eeed40029e2ef28
Kristin(Sijia) Wei <sj.kristinwei@gmail.com>
2018-11-30 06:22:59 -0500

    add the unittest of game24Activity

---
+++ Phase2/gamecentre/app/src/test/java/csc207project/gamecentre/TwentyFourGame/ScoreBoard24GameActivityTest.java
@@ -0,0 +1,12 @@
+package csc207project.gamecentre.TwentyFourGame;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ScoreBoard24GameActivityTest {
+
+    @Test
+    public void onCreate() {
+    }
+}
